2160b92f6a49c534561b4ba02d5ae683a7de0c64
Herney Ruiz <herneyruiz36@gmail.com>
2021-04-18 14:51:15 -0500

    test: add test area square

---
+++ 01-operators/exercise-06/tests/square_test.go
@@ -0,0 +1,20 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/herneygr11/exercise-06/services"
+)
+
+func TestArea(t *testing.T) {
+	square := services.Square{
+		Side: 12,
+	}
+
+	got := 144.0
+	want := square.Area()
+
+	if got != want {
+		t.Errorf("got %.2f, want %.2f", got, want)
+	}
+}
